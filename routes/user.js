373c2ce05f529032d6683a97264a18e7056f5cdc
mohammedrafiqp <mohammedrafiq.p@gmail.com>
2022-05-29 15:15:09 +0530

    updated routes

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var UserRequest = require('../requests/index.req');
+var Request = require('../requests/index.req');
 var UserCtrl = require('../controller/user.ctrl');
 
-router.get('/:id', (req, res) => { UserRequest.getRequest(['user'], UserCtrl, 'getUser', req, res) });
+router.get('/:id', (req, res) => { Request.getRequest(['user'], UserCtrl, 'getUser', req, res) });
 
 module.exports = router;

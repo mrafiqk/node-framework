46eac46a206d4b04e909fc33ca4ffaba97945397
mohammedrafiqp <mohammedrafiq.p@gmail.com>
2022-05-29 15:08:04 +0530

    fixed req

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Request = require('../requests/index.req');
+var UserRequest = require('../requests/index.req');
 var UserCtrl = require('../controller/user.ctrl');
 
 router.get('/:id', (req, res) => { UserRequest.getRequest(['user'], UserCtrl, 'getUser', req, res) });
